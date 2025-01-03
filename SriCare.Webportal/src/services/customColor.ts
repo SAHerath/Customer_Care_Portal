98287c0cc6c64b1f1ed56dc2c71b467f79e0a83d
SAHerath <h.supun.anuradha@gmail.com>
2025-01-03 23:41:20 +0530

    add custom colors

--- SriCare.Webportal/src/services/customColor.ts
+++ SriCare.Webportal/src/services/customColor.ts
@@ -4,10 +4,12 @@ import { createTheme, alpha, getContrastRatio} from '@mui/material/styles';
 declare module '@mui/material/styles' {
   interface Palette {
     gold: Palette['primary'];
+    info: Palette['primary'];
   }
 
   interface PaletteOptions {
     gold?: PaletteOptions['primary'];
+    info?: PaletteOptions['primary'];
   }
 }
 
@@ -15,10 +17,12 @@ declare module '@mui/material/styles' {
 declare module '@mui/material/Button' {
   interface ButtonPropsColorOverrides {
     gold: true;
+    info: true;
   }
 }
 
 const goldBase = '#9D6D38';
+const infoBase = '#1976d2';
 
 export const theme = createTheme({
   palette: {
@@ -27,6 +31,12 @@ export const theme = createTheme({
       light: alpha(goldBase, 0.5),
       dark: alpha(goldBase, 0.9),
       contrastText: getContrastRatio(goldBase, '#fff') > 4 ? '#fff' : '#111',
+    },
+    info: {
+      main: infoBase,
+      light: alpha(infoBase, 0.5),
+      dark: alpha(infoBase, 0.9),
+      contrastText: getContrastRatio(infoBase, '#fff') > 4 ? '#fff' : '#111',
     },
   },
 });
